92bfa7fa200dd04992f0c517a6f7b084ccc29cd2
Cooper <cooperflynn@utexas.edu>
2022-05-12 14:08:02 -0500

    refactor

--- app.js
+++ app.js
@@ -4,13 +4,13 @@
 // when user clicks close-btn remove .open-modal from modal-overlay
 
 const modalBtn = document.querySelector(".modal-btn");
-const modalOverlay = document.querySelector(".modal-overlay");
+const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
 
 modalBtn.addEventListener("click", function () {
-  modalOverlay.classList.toggle("open-modal");
+  modal.classList.toggle("open-modal");
 });
 
 closeBtn.addEventListener("click", function () {
-  modalOverlay.classList.remove("open-modal");
+  modal.classList.remove("open-modal");
 });
